1c5c701298e38198caa1bfaea2e2cebd3cfcfedb
riz <mr.ridwan.thekeeper@gmail.com>
2024-01-31 09:42:26 +0700

    feat(tailwind) add tailwind and vscode prettier configuration

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,21 @@
-import type { Component } from "solid-js"
-
-import logo from "./logo.svg"
-import styles from "./App.module.css"
+import { type Component, For } from "solid-js"
 
 const App: Component = () => {
 	return (
-		<div class={styles.App}>
-			<header class={styles.header}>
-				<img src={logo} class={styles.logo} alt="logo" />
-				<p>
-          Edit <code>src/App.tsx</code> and save to reload.
-				</p>
-				<a
-					class={styles.link}
-					href="https://github.com/solidjs/solid"
-					target="_blank"
-					rel="noopener noreferrer"
-				>
-          Learn Solid
-				</a>
-			</header>
-		</div>
+		<>
+			<p class="text- mx-4 mt-4 text-center text-3xl font-bold text-slate-600">
+				Tailwind Playground
+			</p>
+			<div class="container mx-auto mt-6 flex h-80 flex-row items-center justify-around rounded-xl border-2 border-dashed border-slate-300 p-6">
+				<For each={new Array(4).fill(0)}>
+					{(_, i) => (
+						<div class="h-16 w-16 rounded-lg border-4 border-solid border-pink-500 text-center odd:bg-pink-100 even:bg-pink-300">
+							<span class="text-xs font-semibold">Item {i() + 1}</span>
+						</div>
+					)}
+				</For>
+			</div>
+		</>
 	)
 }
 
